c4ab60e4073f09c899ae2a69103ad15a4643e6f4
dtolhuijs <deisi.bonilla@gmail.com>
2016-06-21 20:11:48 +0200

    Seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,10 @@ Post.create( {  title: "Lamb tajine with chickpeas",  content: "500 gram lamb, 3
 Post.create( {  title: "Pasta Carbonara",             content: "500 gram lamb, 3 potatoes, 2 carrots, 1 onion. Step 1: cut the meat. Step 2:Cut the carrots in dices. Step3: Scrap the potatoes. Step 4: Put everything in a pan a cook it on mediumhigh heat."})
 Post.create( {  title: "Lemon risotto with shrimps",  content: "500 gram lamb, 3 potatoes, 2 carrots, 1 onion. Step 1: cut the meat. Step 2:Cut the carrots in dices. Step3: Scrap the potatoes. Step 4: Put everything in a pan a cook it on mediumhigh heat."})
 Post.create( {  title: "Grande sald with bbq fish",   content: "500 gram lamb, 3 potatoes, 2 carrots, 1 onion. Step 1: cut the meat. Step 2:Cut the carrots in dices. Step3: Scrap the potatoes. Step 4: Put everything in a pan a cook it on mediumhigh heat."})
+
+User.delete_all
+
+User.create( { name: "Ramon",  email: "ramon@test.nl", password: "aaaaaaaa", birthday:"1965-01-01", avatar: "profilemale.jpg", bio:"I LOVE food. I already experimented with different recipes when I was 8 years old. My Favorite cuisine is Italian food."})
+User.create( { name: "Deisi",  email: "deisi@test.nl", password: "bbbbbbbb", birthday:"2000-03-01", avatar: "profilemale.jpg", bio:"I LOVE food. I already experimented with different recipes when I was 8 years old. My Favorite cuisine is Italian food."})
+User.create( { name: "Fiona",  email: "fiona@test.nl", password: "cccccccc", birthday:"1985-01-01", avatar: "profilemale.jpg", bio:"I LOVE food. I already experimented with different recipes when I was 8 years old. My Favorite cuisine is Italian food."})
+User.create( { name: "Jake",   email: "jake@test.nl",  password: "dddddddd", birthday:"1990-01-01", avatar: "profilemale.jpg", bio:"I LOVE food. I already experimented with different recipes when I was 8 years old. My Favorite cuisine is Italian food."})
