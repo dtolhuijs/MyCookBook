a9b77e0bf6b10442f358d0cb2e8e2e79ac418545
dtolhuijs <deisi.bonilla@gmail.com>
2016-06-21 19:57:27 +0200

    seed posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.delete_all
+
+Post.create( {  title: "Lamb tajine with chickpeas",  content: "500 gram lamb, 3 potatoes, 2 carrots, 1 onion. Step 1: cut the meat. Step 2:Cut the carrots in dices. Step3: Scrap the potatoes. Step 4: Put everything in a pan a cook it on mediumhigh heat."})
+Post.create( {  title: "Pasta Carbonara",             content: "500 gram lamb, 3 potatoes, 2 carrots, 1 onion. Step 1: cut the meat. Step 2:Cut the carrots in dices. Step3: Scrap the potatoes. Step 4: Put everything in a pan a cook it on mediumhigh heat."})
+Post.create( {  title: "Lemon risotto with shrimps",  content: "500 gram lamb, 3 potatoes, 2 carrots, 1 onion. Step 1: cut the meat. Step 2:Cut the carrots in dices. Step3: Scrap the potatoes. Step 4: Put everything in a pan a cook it on mediumhigh heat."})
+Post.create( {  title: "Grande sald with bbq fish",   content: "500 gram lamb, 3 potatoes, 2 carrots, 1 onion. Step 1: cut the meat. Step 2:Cut the carrots in dices. Step3: Scrap the potatoes. Step 4: Put everything in a pan a cook it on mediumhigh heat."})
