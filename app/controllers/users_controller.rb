4ab6a8f48e5eaf1815955bb29dae7c6fd5eaab7d
dtolhuijs <deisi.bonilla@gmail.com>
2016-06-22 15:04:14 +0200

    added users to posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,47 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+        @user = User.new
+     end
+
   def show
     @user = User.find(params[:id])
   end
 
+  def edit
+          @user = User.find( params[:id] )
+       end
+
+  def update
+      @user = User.find( params[:id] )
+
+      if @user.update_attributes( user_params )
+            redirect_to @user
+           else
+              render 'edit'
+             end
+          end
+
   def create
-    user_params=
-    params.require( :user).permit( :name, :)
-end
+          user = User.new( user_params )
+
+           if user.save
+              redirect_to users_path
+           else
+              render 'new'
+           end
+        end
+
+  def destroy
+             @user = User.find( params[:id] )
+
+             @user.destroy
+
+             redirect_to users_path
+          end
+
+  def user_params
+   params.require( :user ).permit( :name, :avatar, :email, :password, :bio )
+    end
+  end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,14 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Post.delete_all
 
-Post.create( {  title: "Lamb tajine with chickpeas",  content: "500 gram lamb, 3 potatoes, 2 carrots, 1 onion. Step 1: cut the meat. Step 2:Cut the carrots in dices. Step3: Scrap the potatoes. Step 4: Put everything in a pan a cook it on mediumhigh heat."})
-Post.create( {  title: "Pasta Carbonara",             content: "500 gram lamb, 3 potatoes, 2 carrots, 1 onion. Step 1: cut the meat. Step 2:Cut the carrots in dices. Step3: Scrap the potatoes. Step 4: Put everything in a pan a cook it on mediumhigh heat."})
-Post.create( {  title: "Lemon risotto with shrimps",  content: "500 gram lamb, 3 potatoes, 2 carrots, 1 onion. Step 1: cut the meat. Step 2:Cut the carrots in dices. Step3: Scrap the potatoes. Step 4: Put everything in a pan a cook it on mediumhigh heat."})
-Post.create( {  title: "Grande sald with bbq fish",   content: "500 gram lamb, 3 potatoes, 2 carrots, 1 onion. Step 1: cut the meat. Step 2:Cut the carrots in dices. Step3: Scrap the potatoes. Step 4: Put everything in a pan a cook it on mediumhigh heat."})
+Post.create( {  title: "Lamb tajine with chickpeas",  content: "500 gram lamb, 3 potatoes, 2 carrots, 1 onion. Step 1: cut the meat. Step 2:Cut the carrots in dices. Step3: Scrap the potatoes. Step 4: Put everything in a pan a cook it on mediumhigh heat.", user:ramon})
+Post.create( {  title: "Pasta Carbonara",             content: "500 gram lamb, 3 potatoes, 2 carrots, 1 onion. Step 1: cut the meat. Step 2:Cut the carrots in dices. Step3: Scrap the potatoes. Step 4: Put everything in a pan a cook it on mediumhigh heat.", user:deisi})
+Post.create( {  title: "Lemon risotto with shrimps",  content: "500 gram lamb, 3 potatoes, 2 carrots, 1 onion. Step 1: cut the meat. Step 2:Cut the carrots in dices. Step3: Scrap the potatoes. Step 4: Put everything in a pan a cook it on mediumhigh heat.", user:fiona})
+Post.create( {  title: "Grande sald with bbq fish",   content: "500 gram lamb, 3 potatoes, 2 carrots, 1 onion. Step 1: cut the meat. Step 2:Cut the carrots in dices. Step3: Scrap the potatoes. Step 4: Put everything in a pan a cook it on mediumhigh heat.", user:jake})
 
 User.delete_all
 
-User.create( { name: "Ramon",  email: "ramon@test.nl", password: "aaaaaaaa", birthday:"1965-01-01", avatar: "profilemale.jpg", bio:"I LOVE food. I already experimented with different recipes when I was 8 years old. My Favorite cuisine is Italian food."})
-User.create( { name: "Deisi",  email: "deisi@test.nl", password: "bbbbbbbb", birthday:"2000-03-01", avatar: "profilemale.jpg", bio:"I LOVE food. I already experimented with different recipes when I was 8 years old. My Favorite cuisine is Italian food."})
-User.create( { name: "Fiona",  email: "fiona@test.nl", password: "cccccccc", birthday:"1985-01-01", avatar: "profilemale.jpg", bio:"I LOVE food. I already experimented with different recipes when I was 8 years old. My Favorite cuisine is Italian food."})
-User.create( { name: "Jake",   email: "jake@test.nl",  password: "dddddddd", birthday:"1990-01-01", avatar: "profilemale.jpg", bio:"I LOVE food. I already experimented with different recipes when I was 8 years old. My Favorite cuisine is Italian food."})
+ramon = User.create( { name: "Ramon",  email: "ramon@test.nl", password: "aaaaaaaa", birthday:"1965-01-01", avatar: "profilemale.jpg", bio:"I LOVE food. I already experimented with different recipes when I was 8 years old. My Favorite cuisine is Italian food."})
+deisi = User.create( { name: "Deisi",  email: "deisi@test.nl", password: "bbbbbbbb", birthday:"2000-03-01", avatar: "profilemale.jpg", bio:"I LOVE food. I already experimented with different recipes when I was 8 years old. My Favorite cuisine is Italian food."})
+fiona = User.create( { name: "Fiona",  email: "fiona@test.nl", password: "cccccccc", birthday:"1985-01-01", avatar: "profilemale.jpg", bio:"I LOVE food. I already experimented with different recipes when I was 8 years old. My Favorite cuisine is Italian food."})
+jake  = User.create( { name: "Jake",   email: "jake@test.nl",  password: "dddddddd", birthday:"1990-01-01", avatar: "profilemale.jpg", bio:"I LOVE food. I already experimented with different recipes when I was 8 years old. My Favorite cuisine is Italian food."})
